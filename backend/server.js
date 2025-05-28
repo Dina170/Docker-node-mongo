52ba33fdda831f50b0702b31d6dec55198237a0d
dina <dinaelhadidy966@gmail.com>
2025-05-28 14:16:02 +0300

    Add initial backend setup with Express and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 3000;
+
+mongoose
+  .connect("mongodb://mongo:27017/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("hello from node with MongoDB");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
